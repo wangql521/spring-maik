b6d10396711a5f2ac030f670bbfc62f4ff6f6512
zhanglei521 <937274525@qq.com>
2015-11-23 16:21:12 +0800

    彻底删除

--- src/main/java/com/haohui/springmail/App.java
+++ src/main/java/com/haohui/springmail/App.java
@@ -9,8 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! wanql52113321" );
-        System.out.println( "Hello World! wanql5211" );
-		System.out.println( "Hello World! wanql5211" );
+       
     }
 }
