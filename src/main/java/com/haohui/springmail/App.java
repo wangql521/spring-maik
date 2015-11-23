812e0b1cb7c4054a90081d0a76b01a494da09ddb
zhanglei521 <937274525@qq.com>
2015-11-23 16:03:35 +0800

    提交修改

--- src/main/java/com/haohui/springmail/App.java
+++ src/main/java/com/haohui/springmail/App.java
@@ -10,7 +10,6 @@ public class App
     {
 
         System.out.println( "Hello World! wanql52113321" );
-
         System.out.println( "Hello World! wanql5211" );
 		System.out.println( "Hello World! wanql5211" );
     }
