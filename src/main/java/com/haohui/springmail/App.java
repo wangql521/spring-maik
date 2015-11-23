fd9411ba22b97e80f7668d4c23c43058c58f3bb1
zhanglei521 <937274525@qq.com>
2015-11-23 16:55:17 +0800

    搭搭撒撒

--- src/main/java/com/haohui/springmail/App.java
+++ src/main/java/com/haohui/springmail/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! wanql52113321" );
+        System.out.println( "Hello World! wanql5211332111" );
         System.out.println( "Hello World! wanql5211" );
 		System.out.println( "Hello World! wanql5211" );
     }
